c94d535d6b173943a9cc90be1c0ac417e7ee2fa3
mattyza <mattyza@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-05 11:22:15 +0000

    Tagging version 1.0.0.
    git-svn-id: https://plugins.svn.wordpress.org/archives-by-woothemes/tags/1.0.0@751585 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ archives-by-woothemes.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: Archives by WooThemes
+ * Plugin URI: http://woothemes.com/
+ * Description: Hi, we're a collection of archive functions for WordPress. Use us to display archives of your content in "Timeline" or "Sitemap" format, using either a shortcode, action or template tag.
+ * Author: Matty Cohen | WooThemes
+ * Version: 1.0.0
+ * Author URI: http://woothemes.com/
+ *
+ * @package WordPress
+ * @subpackage Woothemes_Archives
+ * @author Matty
+ * @since 1.0.0
+ */
+
+require_once( 'classes/class-woothemes-archives.php' );
+require_once( 'classes/class-woothemes-archives-timeline.php' );
+require_once( 'classes/class-woothemes-archives-sitemap.php' );
+require_once( 'woothemes-archives-timeline.php' );
+require_once( 'woothemes-archives-sitemap.php' );
+global $woothemes_archives;
+$woothemes_archives = new Woothemes_Archives( __FILE__ );
+$woothemes_archives->version = '1.0.0';
+?>
